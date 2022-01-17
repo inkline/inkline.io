c094c755815ab911c92a46ec140f6857bf3c8aa4
Alex Grozav <alex@grozav.com>
2022-01-17 14:36:39 +0200

    feat: Added ending punctuation to app description meta

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default defineComponent({
             meta: [
                 {
                     name: 'description',
-                    content: 'Inkline is the intuitive UI UX DX Components library that gives you the foundation for building high-quality, accessible, and customizable Vue.js Design Systems'
+                    content: 'Inkline is the intuitive UI UX DX Components library that gives you the foundation for building high-quality, accessible, and customizable Vue.js Design Systems.'
                 }
             ]
         });
