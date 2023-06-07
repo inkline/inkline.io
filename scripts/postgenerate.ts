988be1430084374b06ca9585efc61bf468be6102
Alex Grozav <alex@grozav.com>
2023-06-07 17:13:47 +0300

    fix: add postgenerate script

---
+++ scripts/postgenerate.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require('path');
+const fs = require('fs');
+const glob = require('fast-glob');
+
+const root = path.resolve(__dirname, '..');
+const dist = path.join(root, 'dist');
+
+const srcBlogImages = path.join(root, 'assets', 'images', 'blog');
+const distBlogImages = path.join(root, '.output', 'public', 'assets', 'images', 'blog');
+
+if (!fs.existsSync(distBlogImages)) {
+    fs.mkdirSync(distBlogImages);
+}
+
+glob(`${srcBlogImages}/**/*`).then((files: string[]) => {
+    files.forEach((file) => {
+        const relative = path.relative(srcBlogImages, file);
+        const target = path.join(distBlogImages, relative);
+
+        console.log(`Copying ${path.relative(root, file)} -> ${path.relative(root, target)}`);
+        fs.copyFileSync(file, target);
+    });
+});
