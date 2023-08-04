a439525a480aa3efa60584b3ed8a07d66e36f3eb
Alex Grozav <alex@grozav.com>
2023-08-04 16:32:37 +0300

    feat: trigger deployment

--- scripts/vercel/production.sh
+++ scripts/vercel/production.sh
@@ -3,9 +3,9 @@
 tag=$(git tag --contains HEAD)
 
 if [[ $tag == "latest" ]]; then
-    echo "Deploying to production"
-    exit 1
+    echo "Deploying to production";
+    exit 1;
 else
-    echo "Skipping deployment because the commit is not tagged with 'latest'"
-    exit 0
+    echo "Skipping deployment because the commit is not tagged with 'latest'";
+    exit 0;
 fi
