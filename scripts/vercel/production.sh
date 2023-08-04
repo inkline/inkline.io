2d5c5fcbacb94fd001debee5a1e74a0325238225
Alex Grozav <alex@grozav.com>
2023-08-04 16:28:23 +0300

    feat: add deploy condition script for vercel prod build

---
+++ scripts/vercel/production.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+tag=$(git tag --contains HEAD)
+
+if [[ $tag == "latest" ]]; then
+    echo "Deploying to production"
+    exit 1
+else
+    echo "Skipping deployment because the commit is not tagged with 'latest'"
+    exit 0
+fi
